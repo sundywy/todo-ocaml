8427b7fcfdf99b7d9739a29973d4a862767d49d2
sundy <sundy.yap@gmail.com>
2022-12-05 18:29:38 +0800

    feat: add cmdliner for cli and refactor

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,7 @@
 open Base
 open Stdio
 open Yojson.Safe
+open Cmdliner
 
 let input_text = "/Users/sundyyaputra/Projects/ocaml-projects/todo/.input.json"
 
@@ -13,10 +14,14 @@ type item = {
 }
 [@@deriving yojson { exn = true }]
 
-type todo = AddTask of string | ListTask
-
-let create_task_item id task =
-  { id; task; finish = false; created_at = Unix.time (); completed_at = None }
+let create_task_item id task_str =
+  {
+    id;
+    task = String.concat ~sep:" " task_str;
+    finish = false;
+    created_at = Unix.time ();
+    completed_at = None;
+  }
 
 let to_json_list tasks = `List (List.map ~f:item_to_yojson tasks)
 
@@ -33,19 +38,19 @@ let to_string { task; finish; _ } =
   let complete = if finish then "[x]" else "[ ]" in
   String.concat ~sep:" " [ complete; task ]
 
-let run_task task =
+let list_task () =
   let tasks = read_tasks () in
-  match task with
-  | AddTask todo_task ->
-      add_task tasks (create_task_item (List.length tasks + 1) todo_task)
-  | ListTask -> List.iter ~f:(fun x -> print_endline (to_string x)) tasks
+  List.iter ~f:(fun x -> print_endline (to_string x)) tasks
 
-let get_args () =
-  let args = Sys.get_argv () |> Array.to_list in
-  let texts = List.drop args 1 in
-  if List.is_empty texts then ListTask
-  else AddTask (String.concat ~sep:" " texts)
+let list_task_cmd = Cmd.v (Cmd.info "list") Term.(const list_task $ const ())
 
-let () =
-  let task = get_args () in
-  run_task task
+let add_task task_strings =
+  let tasks = read_tasks () in
+  add_task tasks (create_task_item (List.length tasks + 1) task_strings)
+
+let add_task_cmd =
+  let task = Arg.(non_empty & pos_all string [] & info []) in
+  Cmd.v (Cmd.info "add") Term.(const add_task $ task)
+
+let cmd = Cmd.group (Cmd.info "todo") [ list_task_cmd; add_task_cmd ]
+let () = Caml.exit (Cmd.eval cmd)
